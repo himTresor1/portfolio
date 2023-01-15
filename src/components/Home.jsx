1fd02d06bfd32ac741664f0caad93f2b4235991a
himTresor1 <amizeropeace@gmail.com>
2023-01-15 20:24:28 +0200

    HOme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,21 @@ import React from "react";
 function Home(){
     return(
 
-    <div>
-        <div className="bg-black intro-phrase  w-[45%] h-[550px] "> 
+    <div  className="">
+       <div className="left">
+       <div className="bg-black intro-phrase  w-[45%] h-[550px] "> 
         <h1>BLENDsdfas </h1>
         saldkfjasdfcxvzvcvxcvzv
         </div>
-        <div className="  bg-yellow-400 w-[45%] h-[300px] mini-explanatory">
+        <div className="  bg-yellow-400 w-[45%] h-[400px] mini-explanatory">
 
-        </div>
+        </div> 
+       
+       </div>
+       <div className=" bg-blue-400 right">
+            <h1>sdj</h1>
+
+       </div>
     </div>
 
     )
