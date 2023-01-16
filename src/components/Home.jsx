ace62e1dcc5cfd52ef08fff963aaeac9f963e476
himTresor1 <amizeropeace@gmail.com>
2023-01-16 08:06:46 +0200

    Morning start

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,18 @@ import React from "react";
 function Home(){
     return(
 
-    <div  className="">
-       <div className="left">
-       <div className="bg-black intro-phrase  w-[45%] h-[550px] "> 
-        <h1>BLENDsdfas </h1>
+    <div  className="flex ">
+       <div className="left w-[45%] ">
+       <div className="intro-phrase   h-[550px] "> 
+        <h1 className="text-black text-[12rem] ">BLEND </h1>
         saldkfjasdfcxvzvcvxcvzv
         </div>
-        <div className="  bg-yellow-400 w-[45%] h-[400px] mini-explanatory">
+        <div className="  bg-yellow-400 h-[400px] mini-explanatory">
 
         </div> 
        
        </div>
-       <div className=" bg-blue-400 right">
+       <div className=" bg-blue-400 w-[55%] right">
             <h1>sdj</h1>
 
        </div>
