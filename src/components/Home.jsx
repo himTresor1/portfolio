8bf40bec2e94c1f9fe783619e2b025767e754d3e
himTresor1 <amizeropeace@gmail.com>
2023-01-16 08:57:12 +0200

    HOme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,19 @@ function Home(){
 
     <div  className="flex ">
        <div className="left w-[45%] ">
-       <div className="intro-phrase   h-[550px] "> 
-        <h1 className="text-black text-[12rem] ">BLEND </h1>
-        saldkfjasdfcxvzvcvxcvzv
+
+       <div className="intro-phrase flex flex-col   justify-center  h-[600px]  pl-[5rem]"> 
+        <div className=" flex  justify-start h-[10rem] blend">
+        <h1 className="text-black text-[8rem]  font-semibold w-[40%]   ">blend</h1>
+        <div className="h-[3px] bg-black w-[50%] mt-[8rem] "></div>
+        </div>
+        <div className=" h-[10rem] everything"><h1 className="text-black text-[8rem]  font-bold  ">everything</h1> </div>
+        <div className=" flex  justify-start items-center h-[10rem] ">
+        <div className="h-[3px] bg-black w-[55%] mt-[5rem] "></div>
+        <h1 className="text-black text-[8rem]  font-bold w-[40%]   ">great</h1>
+        </div>
+   
+     
         </div>
         <div className="  bg-yellow-400 h-[400px] mini-explanatory">
 
